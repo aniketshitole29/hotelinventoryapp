58a36c5a0b75cb7f540bbdbce45d51fcb12a32c7
Aniket Shitole <Aniket.Shitole@winjit.com>
2023-08-19 15:19:10 +0530

    Addeding basic files.

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hinv-root',
+  templateUrl: './app.component.html',
+  // template: `<h1>Hello world from inline template!</h1>
+  //   <p>Angular is awesome</p> `,
+  styleUrls: ['./app.component.scss'],
+  // styles: [
+  //   `
+  //     h1 {
+  //       color: blue;
+  //     }
+  //   `,
+  // ],
+})
+export class AppComponent {
+  title = 'hotelinventoryapp';
+}
